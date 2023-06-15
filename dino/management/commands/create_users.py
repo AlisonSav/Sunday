f7235d8f51b93278de529cb6211c3d2fa2ef3248
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-06-15 16:17:38 +0200

    add Faker, bulk_create method

--- dino/management/commands/create_users.py
+++ dino/management/commands/create_users.py
@@ -1,23 +1,25 @@
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand, CommandError
-from random import randint
+from django.core.management.base import BaseCommand
 
-from django.utils.crypto import get_random_string
+from faker import Faker
 
 
 class Command(BaseCommand):
     User = get_user_model()
+    fake = Faker()
 
     def add_arguments(self, parser):
-        parser.add_argument("count", type=int, help="Indicates the number of users to be created")
+        parser.add_argument(
+            "count",
+            type=int,
+            choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            help="Indicates the number of users to be created",
+        )
 
     def handle(self, *args, **options):
+        fake = Faker("en_US")
         User = get_user_model()
-        if 1 < int(options.get("count")) < 10:
-            for i in range(options.get("count")):
-                a = randint(99, 98989898)
-                username = get_random_string(10)
-                user = User(username=username, email="email" + str(a) + "@ex.com", password="Pas1231" + str(a))
-                user.save()
-        else:
-            raise CommandError("Invalid count!")
+        User.objects.bulk_create(
+            User(username=fake.name(), email=fake.email(), password=fake.password())
+            for _ in range(options.get("count"))
+        )
